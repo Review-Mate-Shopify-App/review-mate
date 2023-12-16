5e9addccf39d1a7ccbab86b9e8349a60073794eb
Shreya Maheshwari <shreya.maheshwari.2000@gmail.com>
2023-12-16 13:57:26 +0530

    fix

--- web/frontend/pages/index.jsx
+++ web/frontend/pages/index.jsx
@@ -4,16 +4,12 @@ import { useTranslation } from "react-i18next";
 
 export default function Dashboard() {
   const { t } = useTranslation();
-  const cardStyles = {
-    paddingBottom: "20px",
-    background: "black",
-  };
   return (
     <Page fullWidth>
       <TitleBar title={t("Dashboard.title")} primaryAction={null} />
       <Layout>
         <Layout.Section>
-          <Card style={cardStyles}>
+          <Card>
             <div style={{ padding: "20px" }}>
               <Text as="h2" variant="headingLg">
                 {t("Dashboard.letsStart")}
