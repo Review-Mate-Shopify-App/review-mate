ad9b3b3cca30f9415126632f6bb720163d64d33f
Shreya Maheshwari <shreya.maheshwari.2000@gmail.com>
2023-12-16 16:57:09 +0530

    Dashboard page

--- web/frontend/pages/index.jsx
+++ web/frontend/pages/index.jsx
@@ -1,13 +1,26 @@
-import { Card, Page, Layout, Text } from "@shopify/polaris";
-import { TitleBar } from "@shopify/app-bridge-react";
+import {
+  Card,
+  Page,
+  Layout,
+  Text,
+  LegacyCard,
+  Grid,
+  ProgressBar,
+} from "@shopify/polaris";
 import { useTranslation } from "react-i18next";
+import { starImage } from "../assets";
 
 export default function Dashboard() {
   const { t } = useTranslation();
   return (
     <Page fullWidth>
-      <TitleBar title={t("Dashboard.title")} primaryAction={null} />
+      <div style={{ paddingTop: "16px", paddingBottom: "28px" }}>
+        <Text variant="heading2xl" as="h2">
+          {t("Dashboard.title")}
+        </Text>
+      </div>{" "}
       <Layout>
+        {/* Header */}
         <Layout.Section>
           <Card>
             <div style={{ padding: "20px" }}>
@@ -17,7 +30,146 @@ export default function Dashboard() {
             </div>
           </Card>
         </Layout.Section>
-        <Layout.Section>{/* <ProductsCard /> */}</Layout.Section>
+
+        {/* Analytics */}
+        <Layout.Section>
+          <div style={{ paddingTop: "16px", paddingBottom: "28px" }}>
+            <Text variant="headingXl" as="h3">
+              {t("Dashboard.analytics.title")}
+            </Text>
+          </div>
+
+          <Grid>
+            <Grid.Cell columnSpan={{ xs: 6, sm: 3, md: 3, lg: 6, xl: 6 }}>
+              <LegacyCard
+                title={t("Dashboard.analytics.reviewRequests.title")}
+                sectioned
+              >
+                <Grid>
+                  <Grid.Cell columnSpan={{ xs: 4, sm: 2, md: 2, lg: 4, xl: 4 }}>
+                    <p>{t("Dashboard.analytics.reviewRequests.received")}:</p>
+                    <p style={{ fontSize: "20px", paddingTop: "6px" }}>{0}</p>
+                  </Grid.Cell>
+
+                  <Grid.Cell columnSpan={{ xs: 4, sm: 2, md: 2, lg: 4, xl: 4 }}>
+                    <p>{t("Dashboard.analytics.reviewRequests.sent")}:</p>
+                    <p style={{ fontSize: "20px", paddingTop: "6px" }}>{0}</p>
+                  </Grid.Cell>
+
+                  <Grid.Cell columnSpan={{ xs: 4, sm: 2, md: 2, lg: 4, xl: 4 }}>
+                    <p>{t("Dashboard.analytics.reviewRequests.scheduled")}:</p>
+                    <p style={{ fontSize: "20px", paddingTop: "6px" }}>{0}</p>
+                  </Grid.Cell>
+                </Grid>
+              </LegacyCard>
+            </Grid.Cell>
+
+            <Grid.Cell columnSpan={{ xs: 6, sm: 3, md: 3, lg: 6, xl: 6 }}>
+              <LegacyCard
+                title={t("Dashboard.analytics.productReviews.title")}
+                sectioned
+              >
+                <Grid>
+                  <Grid.Cell columnSpan={{ xs: 6, sm: 3, md: 3, lg: 6, xl: 6 }}>
+                    <p>
+                      {t("Dashboard.analytics.productReviews.overallRating")}:
+                    </p>
+                    <div>
+                      <img width={20} height={20} src={starImage} />
+                      <img width={20} height={20} src={starImage} />
+                      <img width={20} height={20} src={starImage} />
+                      <img width={20} height={20} src={starImage} />
+                      <img width={20} height={20} src={starImage} />
+                    </div>
+                  </Grid.Cell>
+
+                  <Grid.Cell columnSpan={{ xs: 6, sm: 3, md: 3, lg: 6, xl: 6 }}>
+                    <p>{t("Dashboard.analytics.productReviews.total")}:</p>
+                    <p style={{ fontSize: "20px", paddingTop: "6px" }}>{0}</p>
+                  </Grid.Cell>
+                </Grid>
+              </LegacyCard>
+            </Grid.Cell>
+          </Grid>
+        </Layout.Section>
+
+        {/* Product stats */}
+        <Layout.Section>
+          <div style={{ paddingTop: "16px", paddingBottom: "28px" }}>
+            <Text variant="headingXl" as="h3">
+              {t("Dashboard.products.title")}
+            </Text>
+          </div>
+          <Card>
+            <div
+              style={{
+                padding: "20px",
+                display: "flex",
+                flexDirection: "column",
+                gap: "10px",
+              }}
+            >
+              <div
+                style={{ display: "flex", gap: "12px", alignItems: "center" }}
+              >
+                <Text variant="bodyLg" as="p" fontWeight="semibold">
+                  {t("Dashboard.products.rating.5stars")}
+                </Text>
+                <div style={{ width: 225 }}>
+                  <ProgressBar progress={75} tone="highlight" />
+                </div>
+                <span style={{ color: "HighlightText" }}>{75}%</span>
+              </div>
+              <div
+                style={{ display: "flex", gap: "12px", alignItems: "center" }}
+              >
+                <Text variant="bodyLg" as="p" fontWeight="semibold">
+                  {t("Dashboard.products.rating.4stars")}
+                </Text>
+                <div style={{ width: 225 }}>
+                  <ProgressBar progress={75} tone="highlight" />
+                </div>
+                <span style={{ color: "HighlightText" }}>{75}%</span>
+              </div>
+
+              <div
+                style={{ display: "flex", gap: "12px", alignItems: "center" }}
+              >
+                <Text variant="bodyLg" as="p" fontWeight="semibold">
+                  {t("Dashboard.products.rating.3stars")}
+                </Text>
+                <div style={{ width: 225 }}>
+                  <ProgressBar progress={75} tone="highlight" />
+                </div>
+                <span style={{ color: "HighlightText" }}>{75}%</span>
+              </div>
+
+              <div
+                style={{ display: "flex", gap: "12px", alignItems: "center" }}
+              >
+                <Text variant="bodyLg" as="p" fontWeight="semibold">
+                  {t("Dashboard.products.rating.2stars")}
+                </Text>
+                <div style={{ width: 225 }}>
+                  <ProgressBar progress={75} tone="highlight" />
+                </div>
+                <span style={{ color: "HighlightText" }}>{75}%</span>
+              </div>
+
+              <div
+                style={{ display: "flex", gap: "12px", alignItems: "center" }}
+              >
+                <Text variant="bodyLg" as="p" fontWeight="semibold">
+                  {t("Dashboard.products.rating.1star")}
+                </Text>
+                <div style={{ width: 225, marginLeft: "10px" }}>
+                  <ProgressBar progress={75} tone="highlight" />
+                </div>
+                <span style={{ color: "HighlightText" }}>{75}%</span>
+              </div>
+            </div>
+          </Card>
+        </Layout.Section>
       </Layout>
     </Page>
   );
