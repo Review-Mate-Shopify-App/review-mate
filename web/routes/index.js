8b5d5c76ac8b3be584810efcaf14812c97d2e58f
Rahul Bishnoi <rahul.gdr29@gmail.com>
2023-12-17 14:50:17 +0530

    RestAPI created for Reviews like overall rating, ratings for particular product etc

--- web/routes/index.js
+++ web/routes/index.js
@@ -2,8 +2,12 @@ import express from "express";
 
 import productRouter from './productRoutes';
 
+import reviewRouter from './reviewRequest';
+
 const router = express.Router();
 
 router.use("/product", productRouter);
+
+router.use("/review", reviewRouter);
 
 export default router;
